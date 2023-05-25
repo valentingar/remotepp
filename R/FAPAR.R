950aa8b025d3d680560b529dd8879e136440902e
Valentin Gartiser <code@valentingartiser.de>
2023-05-25 15:39:30 +0200

    changed FAPAR calulation. Now the range of NDVI values is calulated for the whole dataset or a mask of vegetation types and not by pixel.

--- R/FAPAR.R
+++ R/FAPAR.R
@@ -5,33 +5,52 @@
 #'
 #'
 #' @param NDVI A raster(stack) of NDVI values
-#' @param FAPAR_max constant, see Wang et al. (2017)
-#' @param FAPAR_min constant, see Wang et al. (2017)
+#' @param vegetation_mask A raster layer with classification of vegetation types.
 #'
+#' @param FAPAR_min constant, see Wang et al. (2017)
+#' @param FAPAR_max constant, see Wang et al. (2017)
+#'
+#' @param quantile_min numeric (0,1) for NDVI_min calculation, see Wang et al. (2017)
+#' @param quantile_max numeric (0,1) for NDVI_max calculation, see Wang et al. (2017)
 #'
 #' @references
 #'
 #' \insertRef{Wang2017}{remotepp} \cr
 #'
+#' @importFrom terra values<-
 #'
 #'@export
 
 calc_FAPAR <- function(NDVI,
-                  FAPAR_max = 0.95,
+                  vegetation_mask = NULL,
                   FAPAR_min = 0.001,
-                  cores = 1){
+                  FAPAR_max = 0.95,
+                  quantile_min = 0.05,
+                  quantile_max = 0.95){
 
   NDVI <- as(NDVI, "SpatRaster")
 
-  NDVI_quantiles <- terra::app(NDVI,
-                               quantile,
-                               probs = c(0.05,0.95),
-                               na.rm = TRUE,
-                               cores = cores)
+  if (is.null(vegetation_mask)){
+    vegetation_mask <- NDVI[[1]]
+    values(vegetation_mask) <- 1
+  } else {
+    vegetation_mask <- as(vegetation_mask, "SpatRaster")
+  }
 
+  NDVI_quantiles <-
+  terra::zonal(NDVI,
+               vegetation_mask,
+               fun = function(x, ...) x) %>%
+    apply(MARGIN = 1, FUN = function(x) quantile(unlist(x[-1]), #first entry is zone identifyer
+                                                 probs = c(quantile_min,
+                                                              quantile_max),
+                                                 na.rm = TRUE))
 
+  NDVI_min <- NDVI_max <- vegetation_mask
+  terra::values(NDVI_min) <- NDVI_quantiles[1, terra::values(vegetation_mask)]
+  terra::values(NDVI_max) <- NDVI_quantiles[2, terra::values(vegetation_mask)]
 
-  FAPAR_out <- (NDVI - NDVI_quantiles[[1]]) * (FAPAR_max - FAPAR_min) / (NDVI_quantiles[[2]] - NDVI_quantiles[[1]]) + FAPAR_min
+  FAPAR_out <- (NDVI - NDVI_min) * (FAPAR_max - FAPAR_min) / (NDVI_max - NDVI_min) + FAPAR_min
   FAPAR_out
 
 }
