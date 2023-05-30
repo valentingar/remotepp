b2839d30f03bbe23c5b4cc3a48e73d7a7cd77f04
Valentin Gartiser <code@valentingartiser.de>
2023-05-30 12:14:25 +0200

    added printing method for rpp_CASA class

--- R/rpp_CASA.R
+++ R/rpp_CASA.R
@@ -58,3 +58,25 @@ validate_rpp_CASA <- function(x) {
   # add tests to check your object is created correctly here
   x
 }
+
+
+### METHODS ###
+#' @exportS3Method
+print.rpp_CASA <- function(
+    x,
+    ...) {
+  n_layers <- length(names(x$NPP))
+  cat("A CASA Model output with", n_layers, "layers: \n")
+  n_print <- min(c(10, n_layers))
+  cat(names(x$NPP)[1:n_print])
+  if(n_layers > n_print){
+    cat("...")
+  }
+  cat("\n")
+  cat("extent: ")
+  cat(terra::ext(x$NPP)[1:4], "(xmin xmax ymin ymax) \n")
+  cat("dimension: ")
+  cat(dim(x$NPP)[1:2], "(x y) \n")
+  cat("resolution: ")
+  cat(terra::res(x$NPP)[1:2], "(x y) \n")
+}
